60815605c362a3936efd8376a26ff98190486a32
Logan Franken <loganfranken@gmail.com>
2015-07-05 12:39:24 -0700

    Listened for hash changes

--- script/main.js
+++ script/main.js
@@ -1,75 +1,83 @@
 (function() {
 
-  var urlHash = window.location.hash.substring(1);
+  // TODO: Handle invalid dates
 
-  var segments = urlHash.split('/');
+  window.onhashchange = displayDateCalculation;
+  displayDateCalculation();
 
-  // URL Format: #/until/{date}
-  if(segments[1] === 'until')
+  function displayDateCalculation()
   {
-    var targetDate = new Date(segments[2]);
-    var daysUntil = calcDayDiff(targetDate, Date.now());
 
-    if(daysUntil < 0) {
-      daysUntil = 0;
+    var urlHash = window.location.hash.substring(1);
+
+    var segments = urlHash.split('/');
+
+    // URL Format: #/until/{date}
+    if(segments[1] === 'until')
+    {
+      var targetDate = new Date(segments[2]);
+      var daysUntil = calcDayDiff(targetDate, Date.now());
+
+      if(daysUntil < 0) {
+        daysUntil = 0;
+      }
+
+      var timeUnit = 'days';
+
+      if(daysUntil === 1) {
+        timeUnit = 'day';
+      }
+
+      displayDayDiff(daysUntil, timeUnit + ' until ' + getDateDisplay(targetDate));
+
+      return;
     }
 
-    var timeUnit = 'days';
+    // URL Format: #/since/{date}
+    if(segments[1] === 'since')
+    {
+      var targetDate = new Date(segments[2]);
+      var daysSince = calcDayDiff(Date.now(), targetDate);
+      displayDayDiff(daysSince, 'days since ' + targetDate.toDateString());
 
-    if(daysUntil === 1) {
-      timeUnit = 'day';
+      return;
     }
 
-    displayDayDiff(daysUntil, timeUnit + ' until ' + getDateDisplay(targetDate));
+    // URL Format: #/between/{date1}/{date2}
+    if(segments[1] === 'between')
+    {
+      var firstDate = new Date(segments[2]);
+      var secondDate = new Date(segments[3]);
+      var daysBetween = calcDayDiff(secondDate, firstDate);
+      displayDayDiff(daysBetween, 'days between ' + firstDate.toDateString() + ' and ' + secondDate.toDateString());
 
-    // TODO: Listen for hash changes
+      return;
+    }
 
-    return;
-  }
+    // URL Format: #/{count}/after/{date1}
+    if(segments[2] === 'after')
+    {
+      var targetDate = new Date(segments[3]);
+      var daysAfter = +segments[1];
+      var dateAfter = new Date();
+      dateAfter.setDate(targetDate.getDate() + daysAfter);
+      displayDayCalc(daysAfter + ' days after ' + targetDate.toDateString() + ' is', dateAfter.toDateString());
 
-  // URL Format: #/since/{date}
-  if(segments[1] === 'since')
-  {
-    var targetDate = new Date(segments[2]);
-    var daysSince = calcDayDiff(Date.now(), targetDate);
-    displayDayDiff(daysSince, 'days since ' + targetDate.toDateString());
+      return;
+    }
 
-    return;
-  }
+    // URL Format: #/{count}/before/{date1}
+    if(segments[2] === 'before')
+    {
+      var targetDate = new Date(segments[3]);
+      var daysBefore = +segments[1];
+      var dateBefore = new Date();
+      dateBefore.setDate(targetDate.getDate() - daysBefore);
+      displayDayCalc(daysBefore + ' days before ' + targetDate.toDateString() + ' is', dateBefore.toDateString());
 
-  // URL Format: #/between/{date1}/{date2}
-  if(segments[1] === 'between')
-  {
-    var firstDate = new Date(segments[2]);
-    var secondDate = new Date(segments[3]);
-    var daysBetween = calcDayDiff(secondDate, firstDate);
-    displayDayDiff(daysBetween, 'days between ' + firstDate.toDateString() + ' and ' + secondDate.toDateString());
+      return;
+    }
 
-    return;
-  }
-
-  // URL Format: #/{count}/after/{date1}
-  if(segments[2] === 'after')
-  {
-    var targetDate = new Date(segments[3]);
-    var daysAfter = +segments[1];
-    var dateAfter = new Date();
-    dateAfter.setDate(targetDate.getDate() + daysAfter);
-    displayDayCalc(daysAfter + ' days after ' + targetDate.toDateString() + ' is', dateAfter.toDateString());
-
-    return;
-  }
-
-  // URL Format: #/{count}/before/{date1}
-  if(segments[2] === 'before')
-  {
-    var targetDate = new Date(segments[3]);
-    var daysBefore = +segments[1];
-    var dateBefore = new Date();
-    dateBefore.setDate(targetDate.getDate() - daysBefore);
-    displayDayCalc(daysBefore + ' days before ' + targetDate.toDateString() + ' is', dateBefore.toDateString());
-
-    return;
   }
 
   function getDateDisplay(date) {
